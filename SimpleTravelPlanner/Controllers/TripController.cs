36615f39328cf7e0683e1f926dc61e9a8f6c14e6
LENOVO-PC\AndresCamilo <andresgalvisr@gmail.com>
2017-04-09 23:00:00 +0300

    initial commit, project structure and simple CRUD

---
+++ SimpleTravelPlanner/Controllers/TripController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleTravelPlanner.Controllers
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public Nullable<System.DateTime> Start { get; set; }
+        public Nullable<System.DateTime> End { get; set; }
+        public string Comment { get; set; }
+    }
+    
+    public class TripController : ApiController
+    {
+        public static List<Trip> trips = new List<Trip>();
+
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return trips;
+        }
+
+        public Trip Get(int id)
+        {
+            return trips.First(c => c.Id == id);
+        }
+
+        public HttpResponseMessage Post(Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                trips.Add(trip);
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, trip);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = trip.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+            if (id != trip.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            trips.Remove(trips.First(c => c.Id == id));
+            trips.Add(trip);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            Trip trip = trips.First(c => c.Id == id);
+            trips.Remove(trip);
+            return Request.CreateResponse(HttpStatusCode.OK, trip);
+        }
+    }
+}
---
+++ SimpleTravelPlanner/Scripts/app/tripCtrl.js
@@ -0,0 +1,76 @@
+﻿
+(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+    app.controller('tripController', ['$scope', '$http', tripController]);
+    var apiRoot = '/api/Trip/';
+    function tripController($scope, $http) {
+
+        $scope.loading = true;
+        $scope.addMode = false;
+
+        $http.get(apiRoot).success(function (data) {
+            $scope.trips = data;
+            $scope.loading = false;
+        })
+        .error(function () {
+            $scope.error = "An Error has occured while loading posts!";
+            $scope.loading = false;
+        });
+
+        $scope.toggleEdit = function () {
+            this.trip.editMode = !this.trip.editMode;
+        };
+
+        $scope.toggleAdd = function () {
+            $scope.addMode = !$scope.addMode;
+        };
+
+        $scope.add = function () {
+            $scope.loading = true;
+            $http.post(apiRoot, this.newtrip).success(function (data) {
+                alert("Added Successfully!!");
+                $scope.addMode = false;
+                $scope.trips.push(data);
+                $scope.loading = false;
+            }).error(function (data) {
+                $scope.error = "An Error has occured while Adding trip! " + data;
+                $scope.loading = false;
+            });
+        };
+
+        $scope.save = function () {
+            alert("Edit");
+            $scope.loading = true;
+            var frien = this.trip;
+            alert(frien);
+            $http.put(apiRoot + frien.Id, frien).success(function (data) {
+                alert("Saved Successfully!!");
+                frien.editMode = false;
+                $scope.loading = false;
+            }).error(function (data) {
+                $scope.error = "An Error has occured while Saving trip! " + data;
+                $scope.loading = false;
+            });
+        };
+
+        $scope.deletetrip = function () {
+            $scope.loading = true;
+            var Id = this.trip.Id;
+            $http.delete(apiRoot + Id).success(function (data) {
+                alert("Deleted Successfully!!");
+                $.each($scope.trips, function (i) {
+                    if ($scope.trips[i].Id === Id) {
+                        $scope.trips.splice(i, 1);
+                        return false;
+                    }
+                });
+                $scope.loading = false;
+            }).error(function (data) {
+                $scope.error = "An Error has occured while Saving trip! " + data;
+                $scope.loading = false;
+            });
+        };
+    }
+})();
